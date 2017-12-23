09785477a0e4560a443ef6b16afaebd9208c2e25
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-23 11:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(anArray,callback){
+  anArray.forEach(callback)
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(anArray,index,callback){
+  anArray[index].forEach(callback)
+
+}
