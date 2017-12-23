965f6296246f13f66d4e80d7cc4604730154d535
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-23 14:58:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(anArray,callback){
+  anArray.forEach(callback)
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element,index,array){
+  array[index] += "changed"
+
+}
